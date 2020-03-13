7c46584a5253b5d03d99b70f394ff638579fd4b6
Victor Macias <juliomacias@gmail.com>
2020-03-13 10:28:12 -0500

    SET RECOVERY SIMPLE WITH NO_WAIT.

--- Reducir_el_mdf.sql
+++ Reducir_el_mdf.sql
@@ -5,3 +5,15 @@ use master
 SELECT  'use [' +d.name +'] DBCC SHRINKDATABASE (['+ m.name +'], 10);'
 FROM sys.master_files m
 INNER JOIN sys.databases d ON m.database_id = d.database_id
+
+
+use master
+SELECT  'use [' +d.name +'] DBCC SHRINKDATABASE (['+ m.name +'], TRUNCATEONLY);'
+FROM sys.master_files m
+INNER JOIN sys.databases d ON m.database_id = d.database_id
+where type_desc='ROWS' and  data_space_id=1
+
+
+SELECT  *-- 'use [' +d.name +'] DBCC SHRINKDATABASE (['+ m.name +'], TRUNCATEONLY);'
+FROM sys.master_files m
+INNER JOIN sys.databases d ON m.database_id = d.database_id
